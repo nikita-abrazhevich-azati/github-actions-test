a4286c4404a85739fe7a505fb148294bde47b445
Nikita <neron2150@gmail.com>
2020-10-12 15:02:12 +0300

    1

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          some changes
+          some changes1d
         </p>
         <a
           className="App-link"
