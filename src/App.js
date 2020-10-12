9af8c2d658a68ea05feab3b6f2990caeb9b0b652
Nikita <neron2150@gmail.com>
2020-10-12 15:45:25 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.asdasdasdas
+          Edit <code>src/App.js</code> and save to reload.asdasdasdasasasd
           some changes3d5
         </p>
         <a
